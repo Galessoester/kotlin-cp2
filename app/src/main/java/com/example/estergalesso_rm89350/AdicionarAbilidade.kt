db3a4218b685d79d11a3f37a1cb92a757577bbb6
Galessoester <galessoester@gmail.com>
2023-05-05 22:52:39 -0300

    primeiro commit

---
+++ app/src/main/java/com/example/estergalesso_rm89350/AdicionarAbilidade.kt
@@ -0,0 +1,11 @@
+package com.example.estergalesso_rm89350
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class AdicionarAbilidade : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_adicionar_abilidade)
+    }
+}
